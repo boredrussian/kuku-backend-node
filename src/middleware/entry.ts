157a378c76c2d879d7aa81812d75421cd1e2b02e
Dmitriy Popovich <front.end.ar.dzencode@gmail.com>
2021-05-13 20:55:05 +0300

    mime initial

--- src/middleware/entry.ts
+++ src/middleware/entry.ts
@@ -1,19 +1,34 @@
-import { Router } from 'express';
-import cors from 'cors';
-import parser from 'body-parser';
-import compression from 'compression';
+import { Router } from "express";
+import cors from "cors";
+import parser from "body-parser";
+import compression from "compression";
+import express from "express";
 
 const handleCors = (router: Router): void => {
-    router.use(cors({ credentials: true, origin: true }));
+  router.use(cors({ credentials: true, origin: true }));
 };
 
 const handleBodyRequestParsing = (router: Router): void => {
-    router.use(parser.urlencoded({ extended: true }));
-    router.use(parser.json({ limit: '150kb' }));
+  router.use(parser.urlencoded({ extended: true }));
+  router.use(parser.json({ limit: "150kb" }));
 };
 
 const handleCompression = (router: Router): void => {
-    router.use(compression());
+  router.use(compression());
 };
 
-export default [handleCors, handleBodyRequestParsing, handleCompression];
+const handleStatic = (router: Router): void => {
+  /**
+   *  console.log("__dirname", __dirname);
+   *  console.log("process.cwd()", process.cwd());
+   *  router.use("/static", express.static("public"));
+   */
+  router.use("/static", express.static(process.cwd() + "/public"));
+};
+
+export default [
+  handleCors,
+  handleBodyRequestParsing,
+  handleCompression,
+  handleStatic,
+];
--- src/service/fileStorage/utilities/saveFiles.ts
+++ src/service/fileStorage/utilities/saveFiles.ts
@@ -7,14 +7,54 @@ import { updateIndex, putFile } from "../../../lib/fs";
 import Busboy from "busboy";
 import path from "path";
 import * as fs from "fs";
+import CryptoJS from "crypto-js";
+import bs58 from "bs58";
+import fsp from "fs/promises";
+import { createHash } from "crypto";
 // import Logger from "../../../lib/logger";
 // const logger = new Logger();
+import { nanoid } from "nanoid";
+
+export const getHash = (data: any) => {
+  const postJson = "string";
+  let resHash;
+  try {
+    const hash = CryptoJS.SHA256(postJson);
+    const hashString = hash.toString(CryptoJS.enc.Hex);
+    const bytes = Buffer.from(hashString, "hex");
+    resHash = bs58.encode(bytes);
+  } catch (e) {
+    console.error("[getHash]", e);
+  }
+  return resHash;
+};
+
+/**
+ *   way to calculate chunks use node lib crypto
+ *   import { createHash } from "crypto";
+ *   const getHash = createHash("sha256");
+ *   getHash.update(data.toString("base64"));
+ *   file.on("data", function (data: any) { getHash.update(data);}
+ *   const hashFinCripto = getHash.copy().digest("hex");
+ *
+ *  or gather chunks in one array
+ *  binaryArr.push(data);
+ *  file.on("data", function (data: any) {
+ *  const buf = Buffer.concat(binaryArr);
+ *  const hash = CryptoJS.SHA256(buf.toString("base64")); }
+ *  const hashString = hash.toString(CryptoJS.enc.Base64);
+ *
+ */
 
 const saveFiles = async (req: Request, res: Response, next: NextFunction) => {
-  console.log("req.body", req.body);
-  const fsPath = process.cwd() + "/src/fileStorage/imgStorage";
+  const fsPath = process.cwd() + "/public";
+  const tempFileNameHash = nanoid();
   const busboy = new Busboy({ headers: req.headers });
-  // const { post, addToIndex } = req.body;
+
+  const sha256 = CryptoJS.algo.SHA256.create();
+
+  let hash_58: string = "alr";
+
   busboy.on(
     "file",
     function (
@@ -24,19 +64,50 @@ const saveFiles = async (req: Request, res: Response, next: NextFunction) => {
       encoding: any,
       mimetype: any
     ) {
-      const saveTo = path.join(fsPath, filename);
+      const fileNameArr = filename.split(".");
+      let nameExtension = fileNameArr[fileNameArr.length - 1];
+      mime.getType("txt"); // ⇨ 'text/plain'
+      mime.getExtension("text/plain");
+      const tempFileName = `${tempFileNameHash}.${nameExtension}`;
+      const saveTo = path.join(fsPath, tempFileName);
+      file.on("data", function (data: any) {
+        sha256.update(data.toString("base64"));
+      });
+
+      file.on("end", async function () {
+        const hashFin = sha256.finalize();
+        const hashString = hashFin.toString(CryptoJS.enc.Hex);
+        const bytes = Buffer.from(hashString, "hex");
+        hash_58 = bs58.encode(bytes);
+        const hashFileName = `${hash_58}.${nameExtension}`;
+
+        try {
+          await fsp.rename(
+            path.join(fsPath, tempFileName),
+            path.join(fsPath, hashFileName)
+          );
+        } catch (e) {
+          console.warn("rename files", e);
+        }
+        // res.end("resHash");
+      });
+
       const stream = fs.createWriteStream(saveTo);
       file.pipe(stream);
-      // stream.on("close", () => {
-      //   console.log(`Upload of '${filename}' finished`);
-      //   res.redirect("back");
-      // });
+
+      stream.on("close", () => {
+        const resData = {
+          hash: hash_58,
+          type: nameExtension,
+        };
+        res.send(resData);
+      });
     }
   );
 
   busboy.on("finish", function () {
-    res.writeHead(200, { Connection: "close" });
-    res.end("That's all folks!");
+    // res.send(hash_58);
+    // res.writeHead(200, { Connection: "close" });
   });
 
   return req.pipe(busboy);
